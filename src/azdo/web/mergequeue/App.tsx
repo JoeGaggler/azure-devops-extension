3411d82cb5ecbf3f45bcb923e5ee490a64bbded8
Joe Gallagher <github@pingmint.net>
2025-06-26 16:49:12 -0400

    get pull requests

--- src/azdo/web/mergequeue/App.tsx
+++ src/azdo/web/mergequeue/App.tsx
@@ -3,6 +3,8 @@ import * as SDK from 'azure-devops-extension-sdk';
 import { IProjectPageService } from "azure-devops-extension-api";
 import { Button } from "azure-devops-ui/Button";
 import { Card } from "azure-devops-ui/Card";
+import { getAzdo } from '../azdo/azdo.ts';
+
 
 interface AppProps {
     bearerToken: string | null;
@@ -27,6 +29,9 @@ function App(p: AppProps) {
         const proj = await projectInfoService.getProject();
         // // let proj = "11"
         console.log("Project:", proj);
+
+        let pullRequests = await getAzdo(`https://dev.azure.com/${host.name}/${proj?.name}/_apis/git/pullrequests?api-version=7.2-preview.2`, p.bearerToken as string);
+        console.log("Pull Requests:", pullRequests);
     }
 
     return (
@@ -35,6 +40,7 @@ function App(p: AppProps) {
                 <div className="flex-row rhythm-horizontal-8 flex-center">
                     <div>Coming soon.</div>
                 </div>
+                <br />
                 <Button
                     text="Debug"
                     primary={true}

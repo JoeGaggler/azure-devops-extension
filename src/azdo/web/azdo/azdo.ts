52c1c604d3e6e4cf95f4f24dfd8f2afd5bf165f3
Joe Gallagher <github@pingmint.net>
2025-06-26 15:56:43 -0400

    get project name

---
+++ src/azdo/web/azdo/azdo.ts
@@ -0,0 +1,48 @@
+export async function getAzdo(url: string, bearertoken: string): Promise<any> {
+    console.log("getapi", url);
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${bearertoken}`,
+            "Content-Type": "application/json",
+            "X-Bearer": bearertoken,
+        }
+    });
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } else {
+        console.log("Error fetching azdo data", response.statusText);
+    }
+}
+
+export async function patchAzdo(url: string, body: any, bearertoken: string): Promise<any> {
+    console.log("patchapi", url, body);
+    const response = await fetch(url, {
+        method: "PATCH",
+        headers: {
+            "Authorization": `Bearer ${bearertoken}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    console.log("patchapi response", response);
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } else {
+        console.log("Error patching azdo data", response.statusText);
+    }
+}
+
+export async function approveRun(approvalId: string, status: string, comment: string, bearertoken: string): Promise<any> {
+    const response = await patchAzdo(`https://dev.azure.com/org/proj/_apis/pipelines/approvals?api-version=7.2-preview.2`, [{
+        approvalId: approvalId,
+        comment: comment,
+        status: status
+    }], bearertoken as string);
+    console.log("approval response: ", response);
+    return response;
+}
--- src/azdo/web/mergequeue/App.tsx
+++ src/azdo/web/mergequeue/App.tsx
@@ -1,3 +1,7 @@
+// import { CommonServiceIds, type IProjectPageService } from 'azure-devops-extension-api';
+import * as SDK from 'azure-devops-extension-sdk';
+import { IProjectPageService } from "azure-devops-extension-api";
+import { Button } from "azure-devops-ui/Button";
 import { Card } from "azure-devops-ui/Card";
 
 interface AppProps {
@@ -10,12 +14,29 @@ function App(p: AppProps) {
         console.log("App props:", p);
     }
 
+    async function doit() {
+        // const navService = await SDK.getService<IHostNavigationService>("ms.vss-features.host-navigation-service");
+        // const proj = navService.getPageRoute()
+
+        const projectInfoService = await SDK.getService<IProjectPageService>(
+            "ms.vss-tfs-web.tfs-page-data-service" // TODO: CommonServiceIds.ProjectPageService
+        );
+        const proj = await projectInfoService.getProject();
+        // // let proj = "11"
+        console.log("Project:", proj);
+    }
+
     return (
         <>
             <Card className="padding-8">
                 <div className="flex-row rhythm-horizontal-8 flex-center">
                     <div>Coming soon.</div>
                 </div>
+                <Button
+                    text="Debug"
+                    primary={true}
+                    onClick={() => { doit(); }}
+                />
             </Card>
         </>
     )

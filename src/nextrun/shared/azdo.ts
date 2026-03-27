7843f838d71b07e835e407b76926eb8c59d227ce
Joe Gallagher <github@pingmint.net>
2026-03-27 14:24:15 -0400

    setup source pipeline document

---
+++ src/nextrun/shared/azdo.ts
@@ -0,0 +1,445 @@
+import * as SDK from 'azure-devops-extension-sdk';
+import { type IProjectPageService } from "azure-devops-extension-api";
+import { type IExtensionDataService } from 'azure-devops-extension-api';
+
+export async function getAzdo(url: string, bearertoken: string): Promise<any> {
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${bearertoken}`,
+            "Content-Type": "application/json",
+            "X-Bearer": bearertoken,
+        }
+    });
+    if (response.ok) {
+        const data = await response.json();
+        const ctok = response.headers.get("x-ms-continuationtoken");
+        if (ctok) {
+            console.log("Continuation token:", ctok);
+        }
+        // console.log(data);
+        return data;
+    } else {
+        console.log("Error fetching azdo data", response.statusText);
+    }
+}
+
+export async function deleteAzdo(url: string, bearertoken: string): Promise<any> {
+    console.log("deleteapi", url);
+    const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+            "Authorization": `Bearer ${bearertoken}`,
+            "Content-Type": "application/json"
+        }
+    });
+    if (response.ok) {
+        // const data = await response.json();
+        // console.log(data);
+        // return data;
+    } else {
+        console.log("Error deleting azdo data", response.statusText);
+    }
+}
+
+export async function patchAzdo(url: string, body: any, bearertoken: string): Promise<any> {
+    console.log("patchapi", url, body);
+    const response = await fetch(url, {
+        method: "PATCH",
+        headers: {
+            "Authorization": `Bearer ${bearertoken}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    console.log("patchapi response", response);
+    if (response.ok) {
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    } else {
+        console.log("Error patching azdo data", response.statusText);
+    }
+}
+
+export async function postAzdo(url: string, body: any, bearertoken: string): Promise<any> {
+    console.log("postapi", url, body);
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${bearertoken}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    if (response.ok) {
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    } else {
+        console.log("Error posting azdo data", response.statusText);
+    }
+}
+
+// https://dev.azure.com/emdat/Emdat/_apis/pipelines/1938/runs/299579?api-version=7.2-preview.1
+export async function getPipelineRun(tenantInfo: TenantInfo, pipelineId: number, runId: number): Promise<PipelineRun | undefined> {
+    try {
+        let bearer = await SDK.getAccessToken()
+        let json = await getAzdo(`https://dev.azure.com/${tenantInfo.organization}/${tenantInfo.project}/_apis/pipelines/${pipelineId}/runs/${runId}?api-version=7.2-preview.1`, bearer as string);
+        if (!json) {
+            return undefined;
+        }
+        let b = json;
+        return {
+            pipelineId: b.id,
+            name: b.name,
+            state: b.state,
+            result: b.result,
+            createdDate: b.createdDate,
+        };
+    }
+    catch (err) {
+        console.error("Error fetching pull requests:", err);
+        return undefined;
+    }
+}
+
+export interface PipelineRun {
+    pipelineId?: number;
+    name?: string;
+    state?: string;
+    result?: string;
+    createdDate?: string;
+}
+
+// _apis/build/builds?$top=100&queryOrder=queueTimeAscending&minTime=2025-08-21T21:38:14.1184379Z&api-version=7.2-preview.7`, bearer as string);
+
+export async function getCommit(tenantInfo: TenantInfo, repoId: string, commitId: string): Promise<any | undefined> {
+    try {
+        let bearer = await SDK.getAccessToken()
+        let json = await getAzdo(`https://dev.azure.com/${tenantInfo.organization}/${tenantInfo.project}/_apis/git/repositories/${repoId}/commits/${commitId}?$api-version=7.2-preview.2`, bearer as string);
+        let comment = json.comment ?? commitId;
+        comment = comment.split('\n')[0];
+        return {
+            comment: comment,
+        }
+    }
+    catch (err) {
+        console.error("Error fetching commit:", err);
+        return undefined;
+    }
+}
+
+export async function getTopRecentBuilds(tenantInfo: TenantInfo): Promise<TopBuild[] | undefined> {
+    try {
+        let bearer = await SDK.getAccessToken()
+        let json = await getAzdo(`https://dev.azure.com/${tenantInfo.organization}/${tenantInfo.project}/_apis/build/builds?$top=1000&queryOrder=queueTimeDescending&api-version=7.2-preview.7`, bearer as string);
+        return json.value.flatMap((b: any): (TopBuild | readonly TopBuild[]) => {
+            return {
+                pipelineId: b.definition?.id,
+                buildId: b.id,
+                buildNumber: b.buildNumber,
+                repositoryName: b.repository?.name,
+                definitionName: b.definition?.name,
+                status: b.status,
+                result: b.result,
+                webUrl: b._links?.web?.href,
+                queueTime: b.queueTime,
+                commitId: b.sourceVersion
+            }
+        });
+    }
+    catch (err) {
+        console.error("Error fetching pull requests:", err);
+        return undefined;
+    }
+}
+
+export interface TopBuild {
+    pipelineId?: number;
+    buildId?: number;
+    buildNumber?: string;
+    repositoryName?: string;
+    definitionName?: string;
+    status?: string;
+    result?: string;
+    webUrl?: string;
+    queueTime?: string;
+    commitId?: string;
+
+    // pullRequestId?: number;
+    // repository?: any;
+    // targetRefName?: string;
+    // title?: string;
+    // createdBy?: any; // TODO: user type
+    // isDraft?: boolean;
+    // creationDate?: string; // ISO date string
+    // autoCompleteSetBy?: boolean
+    // mergeStatus?: string;
+    // reviewers?: Array<any>; // TODO: reviewer type
+}
+
+export async function getAllPullRequests(tenantInfo: TenantInfo): Promise<PullRequest[] | undefined> {
+    try {
+        let bearer = await SDK.getAccessToken()
+        let json = await getAzdo(`https://dev.azure.com/${tenantInfo.organization}/${tenantInfo.project}/_apis/git/pullrequests?api-version=7.2-preview.2`, bearer as string);
+        return json.value
+    }
+    catch (err) {
+        console.error("Error fetching pull requests:", err);
+        return undefined;
+    }
+}
+
+export async function approveRun(approvalId: string, status: string, comment: string, bearertoken: string): Promise<any> {
+    const response = await patchAzdo(`https://dev.azure.com/org/proj/_apis/pipelines/approvals?api-version=7.2-preview.2`, [{
+        approvalId: approvalId,
+        comment: comment,
+        status: status
+    }], bearertoken as string);
+    console.log("approval response: ", response);
+    return response;
+}
+
+export async function deleteSharedDocument(colId: string, docId: string): Promise<boolean> {
+    const accessToken = await SDK.getAccessToken();
+    const extDataService = await SDK.getService<IExtensionDataService>("ms.vss-features.extension-data-service");
+    const dataManager = await extDataService.getExtensionDataManager(SDK.getExtensionContext().id, accessToken);
+
+    try {
+        await dataManager.deleteDocument(colId, docId);
+        console.log("Delete Shared Document: ", colId, docId);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+export async function getOrCreateSharedDocument(colId: string, docId: string, document: any): Promise<any> {
+    const accessToken = await SDK.getAccessToken();
+    const extDataService = await SDK.getService<IExtensionDataService>("ms.vss-features.extension-data-service");
+    const dataManager = await extDataService.getExtensionDataManager(SDK.getExtensionContext().id, accessToken);
+
+    let userFiltersDoc = document
+    try {
+        userFiltersDoc = await dataManager.getDocument(colId, docId);
+        console.log("Get Shared Document: ", colId, docId, userFiltersDoc);
+    } catch {
+        userFiltersDoc.id = docId;
+        userFiltersDoc = await dataManager.createDocument(colId, userFiltersDoc);
+        console.log("New Shared Document: ", colId, docId, userFiltersDoc);
+    }
+
+    return userFiltersDoc;
+}
+
+export async function trySaveSharedDocument<T extends ExtensionDocument>(colId: string, docId: string, document: T): Promise<T | undefined> {
+    const accessToken = await SDK.getAccessToken();
+    const extDataService = await SDK.getService<IExtensionDataService>("ms.vss-features.extension-data-service");
+    const dataManager = await extDataService.getExtensionDataManager(SDK.getExtensionContext().id, accessToken);
+
+    document.id = docId;
+
+    try {
+        console.log("Updating Shared Document: ", colId, docId, document);
+        let next_document = await dataManager.setDocument(colId, document);
+        console.log("Update Shared Document: ", colId, docId, next_document);
+        return next_document;
+    } catch (err) {
+        console.error("Error updating shared document", err);
+        return undefined;
+    }
+}
+
+export async function getOrCreateUserDocument(colId: string, docId: string, document: any): Promise<any> {
+    const accessToken = await SDK.getAccessToken();
+    const extDataService = await SDK.getService<IExtensionDataService>("ms.vss-features.extension-data-service");
+    const dataManager = await extDataService.getExtensionDataManager(SDK.getExtensionContext().id, accessToken);
+
+    let userFiltersDoc = document
+    try {
+        userFiltersDoc = await dataManager.getDocument(colId, docId, { scopeType: "User" });
+        console.log("Get User Document: ", colId, docId, userFiltersDoc);
+    } catch {
+        userFiltersDoc.id = docId;
+        userFiltersDoc = await dataManager.createDocument(colId, userFiltersDoc, { scopeType: "User" });
+        console.log("New User Document: ", colId, docId, userFiltersDoc);
+    }
+
+    return userFiltersDoc;
+}
+
+export async function trySaveUserDocument(colId: string, docId: string, document: any): Promise<boolean> {
+    const accessToken = await SDK.getAccessToken();
+    const extDataService = await SDK.getService<IExtensionDataService>("ms.vss-features.extension-data-service");
+    const dataManager = await extDataService.getExtensionDataManager(SDK.getExtensionContext().id, accessToken);
+
+    document.id = docId;
+
+    try {
+        document = await dataManager.setDocument(colId, document, { scopeType: "User" });
+        console.log("Update User Document: ", colId, docId, document);
+        return true;
+    } catch (err) {
+        console.error("Error updating shared document", err);
+        return false;
+    }
+}
+
+export async function deleteUserDocument(colId: string, docId: string): Promise<boolean> {
+    const accessToken = await SDK.getAccessToken();
+    const extDataService = await SDK.getService<IExtensionDataService>("ms.vss-features.extension-data-service");
+    const dataManager = await extDataService.getExtensionDataManager(SDK.getExtensionContext().id, accessToken);
+
+    try {
+        await dataManager.deleteDocument(colId, docId, { scopeType: "User" });
+        console.log("Delete User Document: ", colId, docId);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+export async function getAzdoInfo() {
+    let newInfo: TenantInfo = {};
+
+    let host = SDK.getHost()
+    newInfo.organization = host.name;
+
+    const projectInfoService = await SDK.getService<IProjectPageService>(
+        "ms.vss-tfs-web.tfs-page-data-service" // TODO: CommonServiceIds.ProjectPageService
+    );
+    const proj = await projectInfoService.getProject();
+    newInfo.project = proj?.name;
+
+    return newInfo;
+}
+
+export async function postPullRequestStatus(
+    bearerToken: string,
+    tenantInfo: TenantInfo,
+    repository: String,
+    pullRequestId: number,
+    state: string,
+    description: string,
+    targetUrl: string = "https://dev.azure.com/pingmint/pingmint-project/_apps/hub/pingmint.pingmint-extension.pingmint-pipeline-mergequeue"
+): Promise<boolean> {
+    try {
+        let body = {
+            "state": state,
+            "description": description,
+            "context": {
+                "name": "merge-queue",
+                "genre": "pingmint"
+            },
+            "targetUrl": targetUrl
+        };
+        let url = `https://dev.azure.com/${tenantInfo.organization}/${tenantInfo.project}/_apis/git/repositories/${repository}/pullRequests/${pullRequestId}/statuses?api-version=7.2-preview.2`;
+        await postAzdo(url, body, bearerToken);
+        return true;
+    }
+    catch (error) {
+        console.error("Error posting pull request status:", error);
+        return false;
+    }
+}
+
+export async function getPullRequest(
+    bearerToken: string,
+    tenantInfo: TenantInfo,
+    repository: String,
+    pullRequestId: number
+): Promise<PullRequest | undefined> {
+    try {
+        let url = `https://dev.azure.com/${tenantInfo.organization}/${tenantInfo.project}/_apis/git/repositories/${repository}/pullRequests/${pullRequestId}?api-version=7.2-preview.2`;
+        let resp = await getAzdo(url, bearerToken);
+        return resp;
+    }
+    catch (error) {
+        console.error("Error fetching pull request:", error);
+        return undefined;
+    }
+}
+
+export async function getPullRequestStatuses(
+    bearerToken: string,
+    tenantInfo: TenantInfo,
+    repository: String,
+    pullRequestId: number
+): Promise<any> {
+    try {
+        let url = `https://dev.azure.com/${tenantInfo.organization}/${tenantInfo.project}/_apis/git/repositories/${repository}/pullRequests/${pullRequestId}/statuses?api-version=7.2-preview.2`;
+        let resp = await getAzdo(url, bearerToken);
+        return resp;
+    }
+    catch (error) {
+        console.error("Error fetching pull request status:", error);
+        return null;
+    }
+}
+
+export async function deletePullRequestStatus(
+    bearerToken: string,
+    tenantInfo: TenantInfo,
+    repository: String,
+    pullRequestId: number,
+    statusId: number
+): Promise<boolean> {
+    try {
+        let url = `https://dev.azure.com/${tenantInfo.organization}/${tenantInfo.project}/_apis/git/repositories/${repository}/pullRequests/${pullRequestId}/statuses/${statusId}?api-version=7.2-preview.2`;
+        await deleteAzdo(url, bearerToken);
+        return false;
+    }
+    catch (error) {
+        console.error("Error deleting pull request status:", error);
+        return false;
+    }
+}
+
+export async function deletePullRequestStatuses(
+    bearerToken: string,
+    tenantInfo: TenantInfo,
+    repository: String,
+    pullRequestId: number) {
+    let statuses = (await getPullRequestStatuses(bearerToken as string, tenantInfo, repository, pullRequestId))
+        .value.filter((s: any) => s.context.genre == "pingmint" && s.context.name == "merge-queue");
+
+    for (let status of statuses) {
+        deletePullRequestStatus(
+            bearerToken as string,
+            tenantInfo,
+            repository,
+            pullRequestId,
+            status.id)
+    }
+}
+
+export interface TenantInfo {
+    organization?: string;
+    project?: string;
+}
+
+export interface PullRequest {
+    pullRequestId?: number;
+    repository?: any;
+    targetRefName?: string;
+    title?: string;
+    status?: string;
+    createdBy?: any; // TODO: user type
+    isDraft?: boolean;
+    creationDate?: string; // ISO date string
+    autoCompleteSetBy?: boolean
+    mergeStatus?: string;
+    reviewers?: Array<any>; // TODO: reviewer type
+}
+
+export interface Repo {
+    id?: string;
+    name?: string;
+    defaultBranch?: string;
+}
+
+export interface ExtensionDocument {
+    id?: string;
+    [key: string]: any; // Allow any additional properties
+}
--- src/nextrun/web/NextRunTab.tsx
+++ src/nextrun/web/NextRunTab.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import * as Azdo from '../shared/azdo.ts';
 import { MessageCard, MessageCardSeverity } from "azure-devops-ui/MessageCard";
 // import * as SDK from 'azure-devops-extension-sdk';
 
 export interface NextRunTabSingleton {
     bearerToken: string;
     appToken: string;
-    
+
     build: any;
     definition: any;
 }
@@ -14,17 +15,62 @@ export interface NextRunTabProps {
     singleton: NextRunTabSingleton;
 }
 
+interface ReducerState {
+    targetPipelines?: TargetPipeline[];
+}
+
+interface ReducerAction {
+    targetPipelines?: TargetPipeline[];
+}
+
+function reducer(state: ReducerState, action: ReducerAction): ReducerState {
+    let next = { ...state };
+
+    if (action.targetPipelines) {
+        next.targetPipelines = action.targetPipelines;
+    }
+
+    return next;
+}
+
 export function NextRunTab(p: NextRunTabProps) {
     console.log("NextRunTab render", p);
+
+    const sourcePipelinesCollectionId = "source-pipelines";
+    let tenantInfo = React.useRef<Azdo.TenantInfo>();
+
+    const [_state, dispatch] = React.useReducer<(state: ReducerState, action: ReducerAction) => ReducerState>(reducer, {})
 
     // initialize the app
     React.useEffect(() => { init() }, []);
     async function init() {
         console.log("NextRunTab -> init");
+
+        let info = await Azdo.getAzdoInfo();
+        console.log("NextRunTab -> tenant", info);
+        tenantInfo.current = info;
+
+        let project = await info.project;
+        let defId = p.singleton.definition?.id;
+        if (!project || !defId) {
+            console.warn("NextRunTab -> missing project or definition id", { project, defId });
+            return;
+        }
+
+        let docId = `project-${project}-pipeline-${defId}`;
+        console.log("NextRunTab -> source pipeline document id", docId);
+
+        let sources: SourcePipelineDocument = {
+            targetPipelines: []
+        }
+        sources = await Azdo.getOrCreateSharedDocument(sourcePipelinesCollectionId, docId, sources)
+        console.log("NextRunTab -> got shared document", sources);
+
+        dispatch({ targetPipelines: sources.targetPipelines });
     }
 
     React.useEffect(() => {
-        let id = setInterval(() => {tick();}, 5000);
+        let id = setInterval(() => { tick(); }, 5000);
         return () => clearInterval(id);
     }, []);
     async function tick() {
@@ -40,4 +86,11 @@ export function NextRunTab(p: NextRunTabProps) {
             __NEXTRUNVERSION__
         </p>
     </>
+}
+
+export interface SourcePipelineDocument {
+    targetPipelines: TargetPipeline[];
+}
+
+export interface TargetPipeline {
 }

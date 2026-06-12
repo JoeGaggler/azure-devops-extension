2e3e7ca9bd68275203753797da7d94bfe86a5f30
Joe Gallagher <github@pingmint.net>
2026-06-12 17:21:11 -0400

    show all active pull requests

--- src/azdo/web/mergequeuebeta/MergeQueueApp.tsx
+++ src/azdo/web/mergequeuebeta/MergeQueueApp.tsx
@@ -1,29 +1,43 @@
-import { Header } from "azure-devops-ui/Components/Header/Header";
-import { TitleSize } from "azure-devops-ui/Components/Header/Header.Props";
-import { IHeaderCommandBarItem } from "azure-devops-ui/Components/HeaderCommandBar/HeaderCommandBar.Props";
-import { Page } from "azure-devops-ui/Components/Page/Page";
 import React from "react";
+
+import { ArrayItemProvider } from "azure-devops-ui/Utilities/Provider";
+import { Header } from "azure-devops-ui/Components/Header/Header";
+import { IHeaderCommandBarItem } from "azure-devops-ui/Components/HeaderCommandBar/HeaderCommandBar.Props";
+import { IListItemDetails, IListRow } from "azure-devops-ui/Components/List/List.Props";
+import { ListItem, ScrollableList } from "azure-devops-ui/Components/List/List";
+import { ListSelection } from "azure-devops-ui/Components/List/ListSelection";
+import { Page } from "azure-devops-ui/Components/Page/Page";
+import { TitleSize } from "azure-devops-ui/Components/Header/Header.Props";
+import { Card } from "azure-devops-ui/Card";
+
 import { getAzdoInfo, getGitClient, TenantInfo } from "./azuredevops";
+
 import { GitPullRequestSearchCriteria, PullRequestStatus, PullRequestTimeRangeType } from "azure-devops-extension-api/Git/Git";
-//import { GitPullRequestSearchCriteria } from "azure-devops-extension-api/Git/Git";
 
 export interface MergeQueueAppSingleton {
     bearerToken: string;
     appToken: string;
 }
 
+interface PullRequestInfo {
+    id: number;
+    title: string;
+}
+
 interface ReducerState {
+    activePullRequests: PullRequestInfo[];
 }
 
 interface ReducerAction {
-
+    activePullRequests?: PullRequestInfo[];
 }
 
 function reducer(state: ReducerState, action: ReducerAction): ReducerState {
     let next = { ...state };
 
-    if (action) {
-        // TODO
+    if (action.activePullRequests) {
+        console.log("MQ: reducer -> updating active pull requests", action.activePullRequests);
+        next.activePullRequests = action.activePullRequests;
     }
 
     return next;
@@ -34,7 +48,8 @@ export function MergeQueueApp(p: { singleton: MergeQueueAppSingleton }) {
     let singleton = React.useRef(p.singleton);
     let gitClient = React.useRef(getGitClient());
 
-    const [_state, _dispatch] = React.useReducer<(state: ReducerState, action: ReducerAction) => ReducerState>(reducer, {
+    const [state, dispatch] = React.useReducer<(state: ReducerState, action: ReducerAction) => ReducerState>(reducer, {
+        activePullRequests: [],
         // TODO: initial reducer state
     })
 
@@ -90,8 +105,17 @@ export function MergeQueueApp(p: { singleton: MergeQueueAppSingleton }) {
                 targetRefName: undefined!,
                 title: undefined!
             };
-            let prs = await git.getPullRequestsByProject(tenantInfo.project, criteria, undefined, undefined, undefined);
-            console.log("MQ: pull requests", prs);
+            let prs1 = await git.getPullRequestsByProject(tenantInfo.project, criteria, undefined, undefined, undefined);
+            let prs2 = prs1.map((pr): PullRequestInfo => {
+                return {
+                    ...pr, // HACK: smuggle full response
+                    id: pr.pullRequestId,
+                    title: pr.title
+                };
+            });
+
+            dispatch({ activePullRequests: prs2 });
+            console.log("MQ: pull requests", prs2);
         } catch (error) {
             console.error("MQ: ticktock -> error occurred", error);
         }
@@ -110,10 +134,73 @@ export function MergeQueueApp(p: { singleton: MergeQueueAppSingleton }) {
                 commandBarItems={renderPageCommandBarItems()}
             />
 
+            <Card
+                className="padding-8 margin-8"
+                contentProps={{ contentPadding: false }}
+                titleProps={{ text: "All Pull Requests", className:"", size: TitleSize.Medium }}
+                headerClassName=""
+                // headerCommandBarItems={[{
+                //     id: "runTargetPipeline",
+                //     text: "Run",
+                //     onActivate: () => { showRunTargetPipelinePanel(); },
+                //     isPrimary: true,
+                //     important: true,
+                //     disabled: !hasSelectedTargetPipeline,
+                // }]}
+            >
+                <PullRequestList pullRequests={state.activePullRequests} />
+            </Card>
+
             <div className="text-neutral-30 flex-row padding-4">
                 <div className="flex-grow"></div>
                 <div>__MERGEQUEUEVERSION__</div>
             </div>
         </Page>
     );
+}
+
+export interface PullRequestListProps {
+    pullRequests: any[]; // TODO: pull request type
+}
+
+export function PullRequestList({ pullRequests }: PullRequestListProps) {
+    console.log("MQ: PullRequestList -> rendering", pullRequests);
+    let listSelection = new ListSelection(true);
+
+    function onSelectRow(row: IListRow<any>) { // TODO: pull request type
+        console.log("NextRunTab -> targetPipelineSelect", row);
+        // dispatch({ selectTargetPipeline: row.data });
+    }
+
+    function renderRow(
+        index: number,
+        item: any, // TODO: pull request type
+        details: IListItemDetails<any>,
+        key?: string
+    ): JSX.Element {
+        console.log("MQ: PullRequestList -> renderRow", index, key, item, details);
+
+        if (!item) { return <></> }
+
+        return <ListItem
+            key={key || "list-item" + index}
+            index={index}
+            details={details}
+        >
+            <div className="flex-row">{item.pullRequestId} - {item.title}</div>
+        </ListItem>
+    }
+
+    return <>
+        <div className="flex-column">
+            <ScrollableList
+                itemProvider={new ArrayItemProvider(pullRequests || [])}
+                selection={listSelection}
+                onSelect={(_evt, listRow) => { onSelectRow(listRow); }}
+                // onActivate={showRunTargetPipelinePanel}
+                renderRow={renderRow}
+                width="100%"
+            />
+        </div>
+    </>
 }
